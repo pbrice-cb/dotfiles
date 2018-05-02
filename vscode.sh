15b66c0d7a2b44cf4c4aee52c65ceffcbb61949e
pbrice <pbrice@carbonblack.com>
2018-05-01 20:27:00 -0600

    adding module to vscode.sh

---
+++ vscode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+code --install-extension ms-python.python
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension mauve.terraform
+code --install-extension lukehoban.go
+code --install-extension ms-vscode.powershell
+code --install-extension aws-scripting-guy.cform
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension DotJoshJohnson.xml
